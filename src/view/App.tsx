cc95ed47863ba89cfbf7961c7eee07dd33b6d0f8
Kurdi Botond <kurdi.boti@gmail.com>
2022-08-29 20:52:06 +0200

    formatting refined, unneccessary files removed

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -19,94 +19,96 @@ import {
   FormControlLabel,
   Checkbox,
   FormGroup,
-  styled
+  styled,
+  Divider,
+  Button
 } from '@mui/material';
-import { useTranslation } from 'react-i18next';
 import styles from './App.styles';
 
 const AppContainer = styled(Stack)(styles);
 
 function App() {
-  const { t } = useTranslation();
-
   return (
-    <AppContainer>
-      <Stack direction="row" spacing={2}>
-        <div>
-          <Stack direction="row" spacing={2}>
-            <Autocomplete
-              options={[]}
-              sx={{ width: 300 }}
-              renderInput={(params: { InputProps: object }) => (
-                <TextField
-                  {...params}
-                  fullWidth
-                  placeholder={t('placeholders.search')}
-                  InputProps={{
-                    ...params.InputProps,
-                    startAdornment: (
-                      <InputAdornment position="start" className="searchIcon">
-                        <SearchIcon data-testid="SearchIcon" />
-                      </InputAdornment>
-                    )
-                  }}
-                />
-              )}
-            />
-            <IconButton>
-              <AddIcon></AddIcon>
-            </IconButton>
-          </Stack>
-          <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            <ListItem
-              secondaryAction={
-                <IconButton edge="end" aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              }>
-              <ListItemButton>
-                <ListItemText primary="Name of notification 1" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem
-              secondaryAction={
-                <IconButton edge="end" aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              }>
-              <ListItemButton>
-                <ListItemText primary="Name of notification 2" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem
-              secondaryAction={
-                <IconButton edge="end" aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              }>
-              <ListItemButton>
-                <ListItemText primary="Name of notification 3" />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </div>
-        <Card sx={{ minWidth: 275 }}>
-          <CardContent>
-            <Typography variant="h5">Details of the notification</Typography>
-            <Typography>City name: Budapest</Typography>
-            <Typography>Current temperature: 33°C</Typography>
-            <Typography>Notification temperature: 10°C</Typography>
-            <Typography>Message: Put on a coat!</Typography>
-            <FormGroup>
-              <FormControlLabel
-                control={<Checkbox defaultChecked />}
-                label="Email: "
-                labelPlacement="start"
+    <AppContainer direction="row" spacing={2}>
+      <div>
+        <Stack direction="row" spacing={2}>
+          <Autocomplete
+            options={[]}
+            sx={{ width: 300 }}
+            renderInput={(params: { InputProps: object }) => (
+              <TextField
+                {...params}
+                fullWidth
+                placeholder={'Search for notification...'}
+                InputProps={{
+                  ...params.InputProps,
+                  startAdornment: (
+                    <InputAdornment position="start" className="searchIcon">
+                      <SearchIcon data-testid="SearchIcon" />
+                    </InputAdornment>
+                  )
+                }}
               />
-            </FormGroup>
-          </CardContent>
-        </Card>
-      </Stack>
+            )}
+          />
+          <Button variant="contained" startIcon={<AddIcon />}>
+            ADD NEW
+          </Button>
+        </Stack>
+        <List>
+          <ListItem
+            secondaryAction={
+              <IconButton edge="end">
+                <DeleteIcon />
+              </IconButton>
+            }
+            disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Name of notification 1" />
+            </ListItemButton>
+          </ListItem>
+          <Divider component="li" />
+          <ListItem
+            secondaryAction={
+              <IconButton edge="end">
+                <DeleteIcon />
+              </IconButton>
+            }
+            disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Name of notification 2" />
+            </ListItemButton>
+          </ListItem>
+          <Divider component="li" />
+          <ListItem
+            secondaryAction={
+              <IconButton edge="end">
+                <DeleteIcon />
+              </IconButton>
+            }
+            disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Name of notification 3" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </div>
+      <Card sx={{ maxHeight: 275 }}>
+        <CardContent>
+          <Typography variant="h5">Details of the notification</Typography>
+          <Typography>City name: Budapest</Typography>
+          <Typography>Current temperature: 33°C</Typography>
+          <Typography>Notification temperature: 10°C</Typography>
+          <Typography>Message: Put on a coat!</Typography>
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Email: "
+              labelPlacement="start"
+            />
+          </FormGroup>
+        </CardContent>
+      </Card>
     </AppContainer>
   );
 }
