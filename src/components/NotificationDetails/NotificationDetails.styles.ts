5fd99876165ea29d9a9424e23cb7d7433f887929
Kurdi Botond <kurdi.boti@gmail.com>
2022-09-25 15:10:24 +0200

    details view style fixed

---
+++ src/components/NotificationDetails/NotificationDetails.styles.ts
@@ -0,0 +1,13 @@
+import {} from '@mui/system';
+
+export default () => ({
+  '& .rootBox': {
+    padding: 10
+  },
+  '& .MuiButtonBase-root': {
+    padding: 0
+  },
+  '& .MuiFormControlLabel-root': {
+    margin: 0
+  }
+});
--- src/components/NotificationDetails/NotificationDetails.tsx
+++ src/components/NotificationDetails/NotificationDetails.tsx
@@ -1,23 +1,40 @@
-import { Checkbox, FormControlLabel, FormGroup, Paper, Typography } from '@mui/material';
+import {
+  Box,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Paper,
+  Typography,
+  styled
+} from '@mui/material';
+
+import styles from './NotificationDetails.styles';
 
 export {};
+const NotificationDetailsRoot = styled(Paper)(styles);
 
 const NotificationDetails = () => {
   return (
-    <Paper role="NotificationDetails">
-      <Typography>Details of the notification</Typography>
-      <Typography>City name: Budapest</Typography>
-      <Typography>Current temperature: 33°C</Typography>
-      <Typography>Notification temperature: 10°C</Typography>
-      <Typography>Message: Put on a coat!</Typography>
-      <FormGroup>
-        <FormControlLabel
-          control={<Checkbox defaultChecked />}
-          label="Email: "
-          labelPlacement="start"
-        />
-      </FormGroup>
-    </Paper>
+    <NotificationDetailsRoot role="NotificationDetails">
+      <Box className="rootBox">
+        <Typography variant="h6" gutterBottom>
+          Details of the notification
+        </Typography>
+        <Typography>City name: Budapest</Typography>
+        <Typography>Current temperature: 33°C</Typography>
+        <Typography>Notification temperature: 10°C</Typography>
+        <Typography>Message: Put on a coat!</Typography>
+        <Box sx={{ display: 'flex' }}>
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Email: "
+              labelPlacement="start"
+            />
+          </FormGroup>
+        </Box>
+      </Box>
+    </NotificationDetailsRoot>
   );
 };
 
